636de4fcd6263cb43e4c5f72d34d4a9c01ef8e16
Angelina26 <gagelganc00@mail.ru>
2024-03-07 11:01:03 +0300

    coin page

--- client/src/components/pages/CoinPage.tsx
+++ client/src/components/pages/CoinPage.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import CoinVidget from '../ui/CoinVidget';
-import { useAppDispatch, useAppSelector } from '../../hooks/useReduxHook';
+import { useAppDispatch } from '../../hooks/useReduxHook';
 import { getCoinsThunkAction } from '../../redux/thunkActions/marketThunkActions';
 
 export default function CoinPage(): JSX.Element {
--- client/src/components/ui/CoinVidget.tsx
+++ client/src/components/ui/CoinVidget.tsx
@@ -1,45 +1,15 @@
 import React, { useEffect, useRef, memo } from 'react';
 import { useParams } from 'react-router-dom';
+import { Image, Text } from '@chakra-ui/react';
 import { useAppSelector } from '../../hooks/useReduxHook';
+import type { CoinType } from '../../types/coinsListApiTypes';
 
 export default function CoinVidget(): JSX.Element {
   const { id } = useParams();
   const data = useAppSelector((state) => state.coins.data?.coins);
   const oneCoin = data?.find((coin) => coin.uuid === id);
-  console.log(oneCoin);
+  // console.log(oneCoin);
 
-  // const container = useRef<HTMLDivElement | null>(null);
-  // console.log(data);
-  // useEffect(() => {
-  //   const currentContainer = container.current;
-
-  //   if (currentContainer) {
-  //     currentContainer.innerHTML = '';
-
-  //     const script = document.createElement('script');
-  //     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
-  //     script.type = 'text/javascript';
-  //     script.async = true;
-  //     script.text = `
-  //         {
-  //           "autosize": true,
-  //           "symbol": "BYBIT:SOLUSDT",
-  //           "interval": "D",
-  //           "timezone": "Etc/UTC",
-  //           "theme": "dark",
-  //           "style": "1",
-  //           "locale": "en",
-  //           "enable_publishing": false,
-  //           "allow_symbol_change": true,
-  //           "calendar": false,
-  //           "support_host": "https://www.tradingview.com"
-  //         }`;
-  //     currentContainer.appendChild(script);
-  //     return () => {
-  //       currentContainer.innerHTML = '';
-  //     };
-  //   }
-  // }, [coin]);
   const widgetRef = useRef(null);
 
   useEffect(() => {
@@ -71,27 +41,20 @@ export default function CoinVidget(): JSX.Element {
   }, [oneCoin?.symbol]); //
 
   return (
-    //   <div
-    //     className="tradingview-widget-container"
-    //     ref={container}
-    //     style={{ height: '100%', width: '100%' }}
-    //   >
-    //     <div
-    //       className="tradingview-widget-container__widget"
-    //       style={{ height: 'calc(100% - 32px)', width: '100%' }}
-    //     />
-    //     <div className="tradingview-widget-copyright">
-    //       <a href="https://www.tradingview.com/" rel="noopener nofollow noreferrer" target="_blank">
-    //         <span className="blue-text">Track all markets on TradingView</span>
-    //       </a>
-    //     </div>
-    //   </div>
-    <div className="tradingview-widget-container" ref={widgetRef}>
-      <div className="tradingview-widget-container__widget" />
-      <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow noreferrer" target="_blank">
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
+    <div>
+      {oneCoin && (
+        <div style={{display: 'flex'}}>
+          <img src={oneCoin.iconUrl} alt={oneCoin.name} style={{ width: '100px' }}/>
+          <h2 style={{color: 'white'}}>{oneCoin.name}</h2>
+        </div>
+      )}
+      <div className="tradingview-widget-container" ref={widgetRef}>
+        <div className="tradingview-widget-container__widget" />
+        <div className="tradingview-widget-copyright">
+          <a href="https://www.tradingview.com/" rel="noopener nofollow noreferrer" target="_blank">
+            <span className="blue-text">Track all markets on TradingView</span>
+          </a>
+        </div>
       </div>
     </div>
   );
