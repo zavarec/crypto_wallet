560134adb81e8fdcd9bd9718f331614ba9f87a98
gmjuffin <gmjuffin@mail.ru>
2024-03-07 09:27:43 +0300

    forDeploy

--- client/src/components/ui/Balance.tsx
+++ client/src/components/ui/Balance.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@chakra-ui/button';
 import React, { useState } from 'react';
 
 function NumberComponent(): JSX.Element {
@@ -9,7 +10,7 @@ function NumberComponent(): JSX.Element {
 
   return (
     <div style={{border: '1px solid gold'}}>
-      <button style={{ color: 'white', paddingLeft: '5px'}} onClick={handleClick}>{numberVisible ? 'Hide balance' : 'Show balance'}</button>
+      <Button style={{ color: 'white', paddingLeft: '5px'}} onClick={handleClick}>{numberVisible ? 'Hide balance' : 'Show balance'}</Button>
       {numberVisible ? (
         <p style={{ color: 'white',paddingLeft: '5px', fontSize: '20px'}}>1023.47$</p>
       ) : (
